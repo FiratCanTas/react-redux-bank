eccc1ab6ee543f3199ebbc56ee444c7a6556a934
Fırat Can Taş <firatcantass@gmail.com>
2024-08-15 18:15:47 +0300

    ✨  add customer features

---
+++ src/features/customers/customerSlice.js
@@ -0,0 +1,11 @@
+const initialCustomerState = {
+  fullName: "",
+  nationalId: "",
+};
+
+export const customerReducer = (state = initialCustomerState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
