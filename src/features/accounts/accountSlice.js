b0950461beb13b82e30136a7608c24092582635d
Fırat Can Taş <firatcantass@gmail.com>
2024-08-15 18:15:54 +0300

    ✨  add account features

---
+++ src/features/accounts/accountSlice.js
@@ -0,0 +1,10 @@
+const initialAccountState = {
+  balance: "",
+  loan: "",
+};
+export const accountReducer = (state = initialAccountState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
