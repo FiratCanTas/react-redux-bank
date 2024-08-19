35faea0e4a42c4708131a11fae2450b530d1b599
Fırat Can Taş <firatcantass@gmail.com>
2024-08-19 11:26:34 +0300

    🌱 complete customer reducer logic and create action creators

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -1,11 +1,39 @@
 const initialCustomerState = {
   fullName: "",
-  nationalId: "",
+  nationalID: "",
+  createdAt: "",
 };
 
 export const customerReducer = (state = initialCustomerState, action) => {
   switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+    case "customer/updateName":
+      return {
+        ...state,
+        fullName: action.payload,
+      };
     default:
       return state;
   }
+};
+
+export const createCustomer = (fullName, nationalID) => {
+  return {
+    type: "customer/createCustomer",
+    payload: {
+      fullName,
+      nationalID,
+      createdAt: new Date().toISOString(),
+    },
+  };
+};
+
+export const updateName = (name) => {
+  return { type: "customer/updateName", payload: name };
 };
