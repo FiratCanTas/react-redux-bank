1c78f68f65a87234213cecd5c2912fa7b4fe6a6d
Fırat Can Taş <firatcantass@gmail.com>
2024-08-20 22:23:36 +0300

    🌱 complete create customer logic

--- src/features/customers/CreateCustomer.js
+++ src/features/customers/CreateCustomer.js
@@ -1,10 +1,16 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createCustomer } from "./customerSlice";
 
 const Customer = () => {
   const [fullName, setFullName] = useState("");
   const [nationalId, setNationalId] = useState("");
+  const dispatch = useDispatch();
 
-  const handleClick = () => {};
+  const handleClick = () => {
+    if (!fullName || !nationalId) return;
+    dispatch(createCustomer(fullName, nationalId));
+  };
 
   return (
     <div>
