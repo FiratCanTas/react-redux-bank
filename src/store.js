2ab55a50839675b5bbc41b76c3ccc57bc7814a78
can.tas <can.tas@bilgeadam.com>
2024-10-15 10:32:12 +0300

    🌱 update store with redux toolkit

--- src/store.js
+++ src/store.js
@@ -1,13 +1,26 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import { accountReducer } from "./features/accounts/accountSlice";
+// import { applyMiddleware, combineReducers, createStore } from "redux";
+// import thunk from "redux-thunk";
+// import { composeWithDevTools } from "redux-devtools-extension";
+import { configureStore } from "@reduxjs/toolkit";
+import accountReducer from "./features/accounts/accountSlice";
 import { customerReducer } from "./features/customers/customerSlice";
-import thunk from "redux-thunk";
 
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+//?Redux Toolkit
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+//?Redux
+// const rootReducer = combineReducers({
+//   account: accountReducer,
+//   customer: customerReducer,
+// });
 
+// const store = createStore(
+//   rootReducer,
+//   composeWithDevTools(applyMiddleware(thunk))
+// );
 export default store;
