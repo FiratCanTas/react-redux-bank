2a860a2505f5e01b0fada044c090cb14514dd7d4
can.tas <can.tas@bilgeadam.com>
2024-10-17 09:19:58 +0300

    🌱 rebuilded with redux toolkit slice

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -1,44 +1,53 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 const initialAccountState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
   isLoading: false,
 };
-export const accountReducer = (state = initialAccountState, action) => {
-  switch (action.type) {
-    case "account/deposit":
-      return {
-        ...state,
-        balance: state.balance + action.payload,
-        isLoading: false,
-      };
-    case "account/withdraw":
-      return { ...state, balance: state.balance - action.payload };
-    case "account/requestLoan":
-      if (state.loan > 0) return state;
-      return {
-        ...state,
-        loan: action.payload.amount,
-        loanPurpose: action.payload.loanPurpose,
-        balance: state.balance + action.payload.amount,
-      };
-    case "account/payLoan":
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: "",
-        balance: state.balance - state.loan,
-      };
-    case "account/convertingCurrency":
-      return {
-        ...state,
-        isLoading: true,
-      };
-    default:
-      return state;
-  }
-};
 
+//?Redux Toolkit
+
+const accountSlice = createSlice({
+  name: "account",
+  initialState: initialAccountState,
+  reducers: {
+    deposit(state, action) {
+      state.balance += action.payload;
+      state.isLoading = false;
+    },
+    withdraw(state, action) {
+      state.balance -= action.payload;
+    },
+    requestLoan: {
+      prepare(amount, purpose) {
+        return {
+          payload: { amount, purpose },
+        };
+      },
+      reducer(state, action) {
+        if (state.loan > 0) return;
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        state.balance = state.balance + action.payload.amount;
+      },
+    },
+    payLoan(state) {
+      state.balance = state.balance - state.loan;
+      state.loan = 0;
+      state.loanPurpose = "";
+    },
+    convertingCurrency(state) {
+      state.isLoading = true;
+    },
+  },
+});
+
+export default accountSlice.reducer;
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
+
+//Basic way to use thunk function
 export const deposit = (amount, currency) => {
   if (currency === "USD") return { type: "account/deposit", payload: amount };
   return async (dispatch, getState) => {
@@ -53,13 +62,49 @@ export const deposit = (amount, currency) => {
   };
 };
 
-export const withdraw = (amount) => {
-  return { type: "account/withdraw", payload: amount };
-};
+//?Redux
+// export const accountReducer = (state = initialAccountState, action) => {
+//   switch (action.type) {
+//     case "account/deposit":
+//       return {
+//         ...state,
+//         balance: state.balance + action.payload,
+//         isLoading: false,
+//       };
+//     case "account/withdraw":
+//       return { ...state, balance: state.balance - action.payload };
+//     case "account/requestLoan":
+//       if (state.loan > 0) return state;
+//       return {
+//         ...state,
+//         loan: action.payload.amount,
+//         loanPurpose: action.payload.loanPurpose,
+//         balance: state.balance + action.payload.amount,
+//       };
+//     case "account/payLoan":
+//       return {
+//         ...state,
+//         loan: 0,
+//         loanPurpose: "",
+//         balance: state.balance - state.loan,
+//       };
+//     case "account/convertingCurrency":
+//       return {
+//         ...state,
+//         isLoading: true,
+//       };
+//     default:
+//       return state;
+//   }
+// };
 
-export const requestLoan = (amount, purpose) => {
-  return { type: "account/requestLoan", payload: { amount, purpose } };
-};
-export const payLoan = () => {
-  return { type: "account/payLoan" };
-};
+// export const withdraw = (amount) => {
+//   return { type: "account/withdraw", payload: amount };
+// };
+
+// export const requestLoan = (amount, purpose) => {
+//   return { type: "account/requestLoan", payload: { amount, purpose } };
+// };
+// export const payLoan = () => {
+//   return { type: "account/payLoan" };
+// };
--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -1,39 +1,69 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 const initialCustomerState = {
   fullName: "",
   nationalID: "",
   createdAt: "",
 };
 
-export const customerReducer = (state = initialCustomerState, action) => {
-  switch (action.type) {
-    case "customer/createCustomer":
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-        nationalID: action.payload.nationalID,
-        createdAt: action.payload.createdAt,
-      };
-    case "customer/updateName":
-      return {
-        ...state,
-        fullName: action.payload,
-      };
-    default:
-      return state;
-  }
-};
+//?Redux Toolkit
 
-export const createCustomer = (fullName, nationalID) => {
-  return {
-    type: "customer/createCustomer",
-    payload: {
-      fullName,
-      nationalID,
-      createdAt: new Date().toISOString(),
+const customerSlice = createSlice({
+  name: "customer",
+  initialState: initialCustomerState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        return {
+          payload: { fullName, nationalID },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = new Date().toISOString();
+      },
     },
-  };
-};
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
 
-export const updateName = (name) => {
-  return { type: "customer/updateName", payload: name };
-};
+export default customerSlice.reducer;
+export const { createCustomer, updateName } = customerSlice.actions;
+
+//?Redux
+// export const customerReducer = (state = initialCustomerState, action) => {
+//   switch (action.type) {
+//     case "customer/createCustomer":
+//       return {
+//         ...state,
+//         fullName: action.payload.fullName,
+//         nationalID: action.payload.nationalID,
+//         createdAt: action.payload.createdAt,
+//       };
+//     case "customer/updateName":
+//       return {
+//         ...state,
+//         fullName: action.payload,
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+// export const createCustomer = (fullName, nationalID) => {
+//   return {
+//     type: "customer/createCustomer",
+//     payload: {
+//       fullName,
+//       nationalID,
+//       createdAt: new Date().toISOString(),
+//     },
+//   };
+// };
+
+// export const updateName = (name) => {
+//   return { type: "customer/updateName", payload: name };
+// };
